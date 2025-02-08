31f8b6455a6990222f3a844fff74c35ebd444e3f
JadhavAnsh <jadhavansh10@gmail.com>
2025-02-08 13:48:13 +0530

    what we learned on the 31 jan and 6 feb

---
+++ 06-02-25/script.js
@@ -0,0 +1,27 @@
+// const str1 = "Hello world!";
+// const num1 = 42;
+// const bool1 = true;
+// const null1 = null;
+// const arr1 = [1, 2, 3];
+// const obj1 = { a: 1, b: 2, c: 3 };
+
+// console.log("String:", str1);
+// console.log("Number:", num1);
+// console.log("Boolean:", bool1);
+// console.log("Null:", null1);
+// console.log("Array:", arr1);
+// console.log("Object:", obj1);
+
+// for(let i = 0; i < str1.length; i++) {
+//     console.log(str1[i]);
+// }
+
+let num1 = 10;
+let num2 = 20;
+
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+console.log("Modulus:", num1 % num2);
+console.log("Exponentiation:", num2 ** num1);
